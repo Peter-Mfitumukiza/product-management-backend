d6908323daf680b299dd48cb759b408844253080
Peter-Mfitumukiza <mfitep6@gmail.com>
2023-12-26 10:33:25 +0200

    db configuration

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,15 @@
-require("dotenv").config(); // load .env variables
-const mysql = require("mysql"); // import mysql2 library
+require("dotenv").config();
 
-// connect to database
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+const config = {
+  server: process.env.SERVER_NAME,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: false,
+    trustServerCertificate: true,
+  },
+};
 
-connection.connect((error) => {
-  if (error) {
-    console.error(error.message)
-  } else {
-    console.log("DB Connected Successfully!")
-  }
-});
-
-module.exports = connection;
+module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 require("dotenv").config()
+require("./config/db")
 const express = require("express") 
 const cors = require("cors")
 
-// const UserRouter = require("./controller/UserController")
+const authRoutes = require("./controllers/auth.controller")
 const productRoutes = require("./controllers/product.controller")
 
 const app = express();
 
 app.use(cors())
 app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.send("Welcome to product management API!")
 })
 
-// app.use("/user", UserRouter)
+app.use("/auth/login", authRoutes)
 app.use("/product", productRoutes)
 
 const PORT = process.env.PORT || 4001
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
+module.exports = app;
